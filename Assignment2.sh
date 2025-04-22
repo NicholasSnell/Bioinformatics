b6283f114ea5a0285fa30be50cccab3d23aedf6c
NicholasSnell <nicholas.j.snell@wmich.edu>
2025-04-22 17:54:04 -0400

    Add files via upload
    Assignment 2 Submission

---
+++ Assignment2.sh
@@ -0,0 +1,18 @@
+greeting="Hello"
+name="Nick"
+echo $greeting $name
+echo "This script is helping you learn to bash"
+mkdir bioinformatics
+mkdir bash_scripting
+cd Downloads
+mv bash_template.txt ~/Home/
+cd
+cd Home
+mv bash_template.txt ~/bash_scripting/
+cd
+mv bash_scripting ~/bioinformatics/
+mv bioinformatics ~/Desktop/
+cd Desktop
+cd bioinformatics
+cd bash_scripting
+mv bash_template.txt bash_template.sh
